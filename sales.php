1cfb26f2845e9ef319bff047a1be3bd03bb22f34
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-05-11 00:28:15 +0200

    Create sales.php
    sales function

---
+++ sales.php
@@ -0,0 +1,6 @@
+function addSale($customerId, $productId, $quantity) {
+    // Check product availability
+    // Deduct sold quantity from inventory
+    // Calculate total price
+    // Insert sale record into database
+}
